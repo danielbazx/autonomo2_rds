026146bd8263efd726ae80a3de0ee6e7f952fc9b
danielbazx <danicadena054@gmail.com>
2025-09-25 02:35:22 +0000

    Configuración de conexión a RDS y fixes en main.py/db.py

--- db.py
+++ db.py
@@ -3,12 +3,12 @@ from fastapi import Depends, FastAPI
 from sqlmodel import SQLModel, Session
 from sqlmodel import create_engine
 
-rds_connectionstring= "postresqlql://postgres:postegres@database-2.cwha0ue2atiq.us-east-1.rds.amazonaws.com:5432/postgres"
+rds_connectionstring= "postgresql://admin1:tY2u2)S8ErCS@database-2.cwha0ue2atiq.us-east-1.rds.amazonaws.com:5432/postgres"
 engine = create_engine(rds_connectionstring, echo=True)
 
-#def create_all_tables(app: FastAPI):
- #   SQLModel.metadata.create_all(engine)
-  #  yield
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
 
 def get_session():
     with Session(engine) as session:
