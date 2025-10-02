dc8804c7a49113f5c0cd5443a8bf19e60bc91f41
danielbazx <150370286+danielbazx@users.noreply.github.com>
2025-10-02 12:27:04 -0500

    Add files via upload

--- db.py
+++ db.py
@@ -7,8 +7,7 @@ import os
 
 load_dotenv()
 
-#rds_connectionstring= "postgresql://admin1:kMW(o-12K525@database-1.cwha0ue2atiq.us-east-1.rds.amazonaws.com:5432/postgres"
-DATABASE_URL = os.getenv("DATABASE_URL")
+rds_connectionstring= "postgresql://admin1:kMW(o-12K525@database-1.cwha0ue2atiq.us-east-1.rds.amazonaws.com:5432/postgres"
 engine = create_engine(DATABASE_URL, echo=True)
 
 def create_all_tables():
