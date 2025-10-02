37391a7dbb69504ab76af3f7e153cccb631f4f79
danielbazx <danicadena054@gmail.com>
2025-10-01 21:26:17 -0500

    cambio1

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from fastapi import Depends, FastAPI
 from sqlmodel import SQLModel, Session
 from sqlmodel import create_engine
 
-rds_connectionstring= "postgresql://admin1:tY2u2)S8ErCS@database-2.cwha0ue2atiq.us-east-1.rds.amazonaws.com:5432/postgres"
+rds_connectionstring= "postgresql://admin1:kMW(o-12K525@database-1.cwha0ue2atiq.us-east-1.rds.amazonaws.com:5432/postgres"
 engine = create_engine(rds_connectionstring, echo=True)
 
 def create_all_tables(app: FastAPI):
